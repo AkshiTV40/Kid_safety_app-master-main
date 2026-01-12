178f4fced9650381ac6a495f04bd7404fd54bb00
AkshiTV40 <tv9502018@gmail.com>
2026-01-11 22:51:47 -0800

    Make videos page dynamic to prevent build errors

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
+
+export const dynamic = 'force-dynamic';
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
